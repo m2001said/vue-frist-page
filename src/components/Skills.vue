fdc680635ef3de9d6bbf80c470deaa166cdfa6cc
Mohamed Said <ms277712@gmail.com>
2023-02-20 20:45:16 +0200

    styling css for skills page

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,5 +1,13 @@
 <template>
-  <h1>dffff;dgh</h1>
+  <div class="skills flex-column">
+    <input placeholder="Enter your skills" type="text" />
+
+    <ul class="flex-column">
+      <li>lakssfhgd</li>
+      <li>lakssfhgd</li>
+      <li>lakssfhgd</li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -9,4 +17,33 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
+<style scoped>
+.skills {
+  margin: 2rem 0;
+}
+.skills input {
+  border: none;
+  width: 85%;
+  font-size: 1.4rem;
+  padding: 5px;
+  color: rgb(56, 10, 45);
+}
+.skills input::placeholder {
+  color: rgb(56, 10, 45);
+}
+.skills ul {
+  gap: 3px;
+  width: 85%;
+  margin: 5px auto;
+  /* background: red; */
+  list-style: none;
+  padding-left: 0;
+}
+.skills ul li {
+  width: 100%;
+  background: rgb(56, 10, 45);
+  font-size: 1.2rem;
+  padding: 5px;
+  color: white;
+}
+</style>
